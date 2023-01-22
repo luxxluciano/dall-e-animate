59c2cb62529f183a66fe37b59b97a5f8828895a5
Luciano Martins Fagundes <lmartinsfagundes@gmail.com>
2023-01-21 23:45:45 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import cv2
+import numpy as np
+from matplotlib.animation import FuncAnimation
+import streamlit.files as files
+
+def main():
+    st.set_page_config(page_title="DALL-E Mini Image Generator", page_icon=":camera:", layout="wide")
+    st.title("DALL-E Mini Image Generator")
+
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file:
+        # Save the uploaded file to a temporary location
+        file_path = files.save(uploaded_file, "uploaded_image.jpg")
+        img = cv2.imread(file_path)
+        st.image(img, width=512)
+        if st.button("Apply Inpainting"):
+            # Apply inpainting (image completion)
+            mask = np.zeros(img.shape[:2], np.uint8)
+            inpainted = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+            st.image(inpainted, width=512)
+
+    if st.button("Create Animation"):
+        # Create an empty figure
+        fig, ax = plt.subplots()
+        xdata, ydata = [], []
+        ln, = plt.plot([], [], 'ro', animated=True)
+
+        def init():
+            ax.set_xlim(0, 2*np.pi)
+            ax.set_ylim(-1, 1)
+            return ln,
+
+        def update(frame):
+            xdata.append(frame)
+            ydata.append(np.sin(frame))
+            ln.set_data(xdata, ydata)
+            return ln,
+
+        ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                            init_func=init, blit=True)
+        st.pyplot(fig)
+
+if __name__ == "__main__":
+    main()
